e5dff675221e111f031391fd4537f2c862c1eaaf
RTSDA <av@rockvilletollandsda.org>
2024-11-21 09:58:06 -0500

    refactor: move sensitive configuration to YTConfig.swift and add example config

--- Config/Config.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum Config {
-    enum YouTube {
-        static let apiKey = "AIzaSyDBXkz9M_WtYrQc1GKI6xUBuwmZ7npfTGo"
-        static let channelId = "UCH3GQ7cC1gvTSEbTSg2jW3Q"
-    }
-}
