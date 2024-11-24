db009ed8e7538f362d3a6fd02e47a7e1f1d5b483
RTSDA <av@rockvilletollandsda.org>
2024-11-23 20:37:13 -0500

    chore: remove iOS code from Android repo
    - Move iOS code to dedicated RTSDA-iOS repository
    - Clean up Android repository structure

--- ios/RTSDA/Config/Config.example.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum Config {
-    enum YouTube {
-        static let apiKey = "YOUR_YOUTUBE_API_KEY_HERE"
-        static let channelId = "YOUR_YOUTUBE_CHANNEL_ID_HERE"
-    }
-}
--- ios/RTSDA/Extensions/Double+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension Double {
-    func toLong() -> Int64 {
-        Int64(self)
-    }
-}
